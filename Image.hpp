19c580699884899f9f8338461f31179465c1a23f
netolcc06 <netolcc06@gmail.com>
2016-02-15 16:35:27 -0300

    camera parameters

--- Image.hpp
+++ Image.hpp
@@ -119,19 +119,20 @@ class Image
 			file << width_ << " " << height_ << std::endl;
 			file << "255" << std::endl;
 
-			Hitable *list[4];
+			Hitable *list[5];
 			//float R = cos(M_PI / 4);
-			list[0] = new Sphere(Vec3<float>(-1,0.0,-1.0), 0.5, new Lambertian(Vec3<float>(0.1,0.2,0.5)));
+			list[0] = new Sphere(Vec3<float>(-1.5,0.0,-1.0), 0.5, new Lambertian(Vec3<float>(0.1,0.2,0.5)));
 			list[1] = new Sphere(Vec3<float>(1, -100.5, -1.0), 100, new Lambertian(Vec3<float>(0.8, 0.8, 0)));
-			list[2] = new Sphere(Vec3<float>(1.0, 0.0, -1.0), 0.5, new Metal(Vec3<float>(0.8, 0.6, 0.2),1.0));
-			//list[3] = new Sphere(Vec3<float>(-1.0, 0.0, -1.0), 0.5, new Dieletric(1.5));
+			list[2] = new Sphere(Vec3<float>(1.5, 0.0, -1.0), 0.5, new Metal(Vec3<float>(0.8, 0.6, 0.2),1.0));
+			list[3] = new Sphere(Vec3<float>(0.0, 0.0, -1.0), 0.5, new Dieletric(1.5));
+			list[4] = new Sphere(Vec3<float>(0.0, 0.0, -1.0), -0.5, new Dieletric(1.5));
 			//list[4] = new Sphere(Vec3<float>(-1.0, 0.0, -5.0), 3.5, new Lambertian(Vec3<float>(0.6, 0.4, 0.5)));
 			//list[5] = new Sphere(Vec3<float>(-1.0, 0.0, -1.0), 0.5, new Dieletric(10.5));
 
-			Hitable * world = new Hitable_list(list, 3);
+			Hitable * world = new Hitable_list(list, 5);
 			//Hitable *world = random_scene();
 
-			Vec3<float> lookfrom(0.0, 0.0, 2.0), lookat(0.0, 0.0, -1.0);
+			Vec3<float> lookfrom(0.0, 0.0, 1.0), lookat(0.0, 0.0, -1.0);
 			float dist_to_focus = (lookfrom - lookat).length();
 			float aperture = 2.0;
 
