4d43a274d759f7f107422b14a6fe527a402f55ba
José Neto <netolcc06@gmail.com>
2017-09-12 11:00:24 -0300

    Update Triangle.hpp

--- Triangle.hpp
+++ Triangle.hpp
@@ -31,13 +31,11 @@ bool Triangle::hit(Ray<float> & r, float t_min, float t_max, hit_record & rec) c
 
 	float thit, t, u, v;
 
-	//Vec3f v0v1 = v1_ - v0;
 	Vec3<float> v0v1 = v1_ - v0_;
-	//Vec3f v0v2 = v2 - v0;
 	Vec3<float> v0v2 = v2_ - v0_;
-	//Vec3f pvec = dir.crossProduct(v0v2);
+	
 	Vec3<float> pvec = Vec3<float>::cross(r.direction(), v0v2);
-	//float det = v0v1.dotProduct(pvec);
+	
 	float det = Vec3<float>::dot(pvec, v0v1);
 	float kEpsilon = 0.00001;
 
@@ -45,41 +43,28 @@ bool Triangle::hit(Ray<float> & r, float t_min, float t_max, hit_record & rec) c
 	// if the determinant is close to 0, the ray misses the triangle
 	if (det < kEpsilon) return false;
 
-	// ray and triangle are parallel if det is close to 0
-	//if (std::fabs(det) < kEpsilon) return false;
-
 	float invDet = 1 / det;
 	
-	//Vec3f tvec = orig - v0;
 	Vec3<float> tvec = r.origin() - v0_;
-	//u = tvec.dotProduct(pvec) * invDet;
 	u = Vec3<float>::dot(tvec, pvec) * invDet;
 	
 	if (u < 0 || u > 1) return false;
 
-	//Vec3f qvec = tvec.crossProduct(v0v1);
 	Vec3<float> qvec = Vec3<float>::cross(tvec, v0v1);
-	//v = dir.dotProduct(qvec) * invDet;
 	v = Vec3<float>::dot(r.direction(), qvec) * invDet;
 	if (v < 0 || u + v > 1) return false;
 
-	//std::cout << "u = " << u << "v = "<< v << std::endl; system("pause");
-
-	//t = v0v2.dotProduct(qvec) * invDet;
 	t = Vec3<float>::dot(v0v2, qvec) * invDet;
 
 	
 	if (t < 0) return false;// std::cout << t << std::endl;
 
 	rec.p = r.point_at_parameter(t);
-	/*rec.p = Vec3<float>(t*v0_.x() + u * v1_.x() + v*v2_.x(),
-		t*v0_.y() + u * v1_.y() + v*v2_.y(),
-		t*v0_.z() + u * v1_.z() + v*v2_.z());//v0_*t + v1_*u + v2_*v;*/
+
 	rec.t = t;
-	//if(Vec3<float>::dot(normal, r.direction()))
 	rec.normal = normal;
 	rec.mat_ptr = material;
-	//std::cout << "hits" << std::endl;
+	
 	return true;
 }
 
